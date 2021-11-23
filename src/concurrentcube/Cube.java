301d067aea97e112088e2009c042d6b94fb9223f
Karolina <karolina.laskowska19@gmail.com>
2021-11-23 13:17:18 +0100

    dodanie funkcji pomocniczych do obrotow

--- src/concurrentcube/Cube.java
+++ src/concurrentcube/Cube.java
@@ -32,19 +32,91 @@ public class Cube {
         this.afterShowing = afterShowing;
     }
 
-    private int[][] copySide(int side) {
-        int[][] copy = new int[size][size];
-        for (int row = 0; row < size; row++) {
-            for (int column = 0; column < size; column++)
-                copy[row][column] = squares[side][row][column];
+    private int[] copyRow(int side, int row) {
+        int[] copy = new int[size];
+        for (int column = 0; column < size; column++) {
+            copy[column] = squares[side][row][column];
         }
         return copy;
     }
 
-    // rotates only top squares on the side
+    private int[] copyColumn(int side, int column) {
+        int[] copy = new int[size];
+        for (int row = 0; row < size; row++) {
+            copy[row] = squares[side][row][column];
+        }
+        return copy;
+    }
+
+    private int[][] copySide(int side) {
+        int[][] copy = new int[size][size];
+        for (int row = 0; row < size; row++) {
+            copy[row] = copyRow(side, row);
+        }
+        return copy;
+    }
+
+    private void rewriteColumn(int sideFrom, int sideTo, int columnFrom, int columnTo) {
+        for (int row = 0; row < size; row++) {
+            squares[sideTo][row][columnTo] = squares[sideFrom][row][columnFrom];
+        }
+    }
+
+    private void rewriteColumnConversely(int sideFrom, int sideTo, int columnFrom, int columnTo) {
+        for (int row = 0; row < size; row++) {
+            squares[sideTo][row][columnTo] = squares[sideFrom][size - 1 - row][columnFrom];
+        }
+    }
+
+    private void rewriteRow(int sideFrom, int sideTo, int rowFrom, int rowTo) {
+        for (int column = 0; column < size; column++) {
+            squares[sideTo][rowTo][column] = squares[sideFrom][rowFrom][column];
+        }
+    }
+
+    private void rewriteRowConversely(int sideFrom, int sideTo, int rowFrom, int rowTo) {
+        for (int column = 0; column < size; column++) {
+            squares[sideTo][rowTo][column] = squares[sideFrom][rowFrom][size - 1 - column];
+        }
+    }
+
+    private void rewriteRowToColumn(int sideFrom, int sideTo, int rowFrom, int columnTo) {
+        for (int row = 0; row < size; row++) {
+            squares[sideTo][row][columnTo] = squares[sideFrom][rowFrom][row];
+        }
+    }
+
+    private void rewriteRowToColumnConversely(int sideFrom, int sideTo, int rowFrom, int columnTo) {
+        for (int row = 0; row < size; row++) {
+            squares[sideTo][row][columnTo] = squares[sideFrom][rowFrom][size - 1 - row];
+        }
+    }
+
+    private void rewriteColumnToRow(int sideFrom, int sideTo, int columnFrom, int rowTo) {
+        for (int column = 0; column < size; column++) {
+            squares[sideTo][rowTo][column] = squares[sideFrom][column][columnFrom];
+        }
+    }
+
+    private void rewriteColumnToRowConversely(int sideFrom, int sideTo, int columnFrom, int rowTo) {
+        for (int column = 0; column < size; column++) {
+            squares[sideTo][rowTo][column] = squares[sideFrom][size - 1 - column][columnFrom];
+        }
+    }
+
+    // right rotates only top squares on the side
     // this operation is only for rotate(side, 0)
-    private void rotateSide(int side) {
-        int colour;
+    private void rightRotateSide(int side) {
+        int[][] copySide =  copySide(side);
+        for (int row = 0; row < size; row++) {
+            for (int column = 0; column < size; column++)
+                squares[side][row][column] = copySide[size - 1 - column][row];
+        }
+    }
+
+    // left rotates only top squares on the side
+    // this operation is only for rotate(side, size - 1)
+    private void leftRotateSide(int side) {
         int[][] copySide =  copySide(side);
         for (int row = 0; row < size; row++) {
             for (int column = 0; column < size; column++)
@@ -52,16 +124,32 @@ public class Cube {
         }
     }
 
-    // rotates horizontal layer looking from side 0
+    // rotates layer looking from side 0
     // (without squares on side 0 and side 5)
-    private void rotateLayerHorizontal0(int layer) {
-        int colourTemp;
-        for (int column = 0; column < size; column++) {
-            colourTemp = squares[1][layer][column];
-            for (int i = 0; i < 4; i++) {
-                
-            }
+    private void rotatePerimeterLayer0(int layer) {
+        int[] rowTemp = copyRow(1, layer);
+        for (int i = 1; i < 4; i++) {
+            rewriteRow(i + 1, i, layer, layer);
+        }
+        squares[4][layer] = rowTemp;
+    }
+
+    // rotates layer looking from side 1
+    // (without squares on side 1 and side 3)
+    private void rotatePerimeterLayer1(int layer) {
+        int[] columnTemp = copyColumn(0, layer);
+        rewriteColumnConversely(4, 0, size - 1 - layer, layer);
+        rewriteColumnConversely(5, 4, layer, size - 1 - layer);
+        rewriteColumn(2, 5, layer, layer);
+        for (int row = 0; row < size; row++) {
+            squares[2][row][layer] = columnTemp[row];
         }
     }
 
+    // rotates layer looking from side 1
+    // (without squares on side 1 and side 3)
+    private void rotatePerimeterLayer2(int layer) {
+        int[] columnTemp = copyColumn(1, size - 1 - layer);
+        rewriteRowToColumn();
+    }
 }
